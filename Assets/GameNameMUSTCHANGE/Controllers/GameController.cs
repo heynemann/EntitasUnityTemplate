ef7cf45b85576841acda6cf1c48fac828204aefc
Bernardo Heynemann <heynemann@gmail.com>
2017-08-08 23:59:13 -0300

    Entitas Template

---
+++ Assets/GameNameMUSTCHANGE/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	private Systems _systems;
+
+	// Use this for initialization
+	void Start ()
+	{
+		var contexts = Contexts.sharedInstance;
+//		_systems = new Feature("Systems").Add(
+			// Add your features here			
+//		);
+
+		_systems.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_systems.Execute();
+		_systems.Cleanup();
+	}
+}
